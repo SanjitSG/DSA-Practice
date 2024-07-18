16ad46df1df0696c68d6c10568d1272ed250ad74
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-07-18 22:28:50 +0530

    basic practice programs

---
+++ practice.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+  // * 1. find sum and avg of two numbers;
+
+  // int x, y;
+  // cin >> x >> y;
+  // cout << "Sum of " << x << " + " << y << " = " << x + y << endl;
+  // cout << "Average of " << x << " and " << y << " = " << (x + y) / 2;
+
+  // *2. even and odd
+  // *3. quadratic equation
+  // *4. factorial of a number
+  // *5. fibonacci series
+  // *6. area of a circle
+  // *7. sum of elements of an array
+  // *8. find the largest element of an array
+  // *9. implement default arguments
+  // *10. calculator using inline functions
+  // *11. implement static variables
+  return 0;
+}
