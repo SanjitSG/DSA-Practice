6ff5a06863c6e190341cea30ccb0dd5681b59477
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-07-20 01:22:22 +0530

    for-while-loops

---
+++ 6-for-while-loops.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+  // TODO - Print a statement 5 times using for loop
+  /*
+  // int i;
+  for (int i = 1; i <= 5; i = i + 1)
+  {
+    cout << "Sanjit " << i << endl;
+  }
+*/
+
+  // * while loop
+  /*
+    int i = 1;
+    while (i <= 5)
+    {
+      cout << "Sam " << i << endl;
+
+      i = i + 1;
+    }
+
+    */
+
+  // * do-while loop executes at least once regardless of condition
+
+  int i = 2;
+  do
+  {
+    cout << "Man " << i << endl;
+
+    i = i + 1;
+  } while (i <= 1);
+
+  return 0;
+}
