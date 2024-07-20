26b97b2935cd01eb1e89a8b52c407e3c0e0638b0
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-07-21 02:04:56 +0530

    Time and Space complexity

--- 8-time-and-space-complexities.cpp
+++ 8-time-and-space-complexities.cpp
@@ -1,3 +1,15 @@
 // TODO - Time and Space complexity
 
-// * What is time complexity?
+/* What is time complexity?
+ -> The rate at which the time, required to run the code, changes w.r.t the input size, is considered as time complexity.
+
+ 3 Rules -
+ 1. Calculate the time complexity for the worst-case scenario.
+ 2. Avoid including the constant terms.
+ 3.Avoid the lower values.
+
+Big-O notation
+
+What is space complexity?
+-> Auxiliary space + input space.
+ */
