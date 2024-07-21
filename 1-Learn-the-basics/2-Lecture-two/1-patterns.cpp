e9940888d91ddcd5fd66a119ec898c0528a29baa
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-07-22 01:33:55 +0530

    pattern 1-3

--- 1-Learn-the-basics/2-Lecture-two/1-patterns.cpp
+++ 1-Learn-the-basics/2-Lecture-two/1-patterns.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 // * this is how backend looks like in an online compiler. - Test cases
+
 void print1(int n)
 {
   for (int i = 0; i < n; i++)
@@ -15,6 +16,29 @@ void print1(int n)
   }
 }
 
+void print2(int n)
+{
+  for (int i; i < n; i++)
+  {
+    for (int j = 0; j <= i; j++)
+    {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+}
+
+void print3(int n)
+{
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+}
 int main()
 {
 
@@ -24,7 +48,7 @@ int main()
   {
     int n;
     cin >> n;
-    print1(n);
+    print3(n);
   }
 
   return 0;
