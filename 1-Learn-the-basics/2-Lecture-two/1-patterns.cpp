28f64e828120dec733c079785a0669be75152c7d
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-07-22 00:19:06 +0530

    pattern printing function - online compiler - test cases

--- 1-Learn-the-basics/2-Lecture-two/1-patterns.cpp
+++ 1-Learn-the-basics/2-Lecture-two/1-patterns.cpp
@@ -2,16 +2,29 @@
 
 using namespace std;
 
-int main()
+// * this is how backend looks like in an online compiler. - Test cases
+void print1(int n)
 {
-
-  for (int i = 0; i < 4; i++)
+  for (int i = 0; i < n; i++)
   {
-    for (int j = 0; j < 4; j++)
+    for (int j = 0; j < n; j++)
     {
       cout << " * ";
     }
     cout << endl;
+  }
+}
+
+int main()
+{
+
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; i++)
+  {
+    int n;
+    cin >> n;
+    print1(n);
   }
 
   return 0;
