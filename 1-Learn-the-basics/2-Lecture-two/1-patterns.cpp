bc4a377c9355542d1f4e6670bf43d7886104e1b3
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-07-23 22:57:24 +0530

    pattern

--- 1-Learn-the-basics/2-Lecture-two/1-patterns.cpp
+++ 1-Learn-the-basics/2-Lecture-two/1-patterns.cpp
@@ -80,7 +80,7 @@ void print7(int n)
 {
   for (int i = 0; i < n; i++)
   {
-    for (int j = 0; j < 2 * n - 1; j++)
+    for (int j = 0; j < n - i + 1; j++)
     {
       cout << " ";
     }
@@ -90,7 +90,7 @@ void print7(int n)
       cout << "*";
     }
 
-    for (int j = 0; j < 2 * n - 1; j++)
+    for (int j = 0; j < n - i + 1; j++)
     {
       cout << " ";
     }
