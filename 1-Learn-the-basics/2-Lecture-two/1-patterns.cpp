d5de03d1424a4db2f1381e5ac17e98f53336c8c3
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-07-24 20:54:42 +0530

    pattern -8

--- 1-Learn-the-basics/2-Lecture-two/1-patterns.cpp
+++ 1-Learn-the-basics/2-Lecture-two/1-patterns.cpp
@@ -97,6 +97,29 @@ void print7(int n)
     cout << endl;
   }
 }
+
+void print8(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < i; j++)
+    {
+      cout << " ";
+    }
+
+    for (int j = 0; j < (2 * n) - (2 * i + 1); j++)
+    {
+      cout << "*";
+    }
+
+    for (int j = 0; j < i; j++)
+    {
+      cout << " ";
+    }
+    cout << endl;
+  }
+}
+
 int main()
 {
 
@@ -106,7 +129,7 @@ int main()
   {
     int n;
     cin >> n;
-    print7(n);
+    print8(n);
   }
 
   return 0;
