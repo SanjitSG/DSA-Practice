ac01a75d8b5a4fd90dced28fd9dfbf4a5bcec166
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-07-30 01:27:41 +0530

    palindrome

--- 1-Learn-the-basics/4-Lecture-four/basicmath.cpp
+++ 1-Learn-the-basics/4-Lecture-four/basicmath.cpp
@@ -4,20 +4,57 @@ using namespace std;
 
 void countDigit(int n)
 {
-  while (n > 0)
-  {
-    int lastDigit = n % 10;
-    cout << lastDigit << " ";
-    n = n / 10;
-  }
+  int cnt;
+  /* while (n > 0)
+   {
+     cnt += 1;
+     n = n / 10;
+   }
+   cout << cnt; */
+
+  cnt = log10(n) + 1;
+  cout << cnt;
+
+  // Time complexity => O( log10(n) )
 };
 
+void revDigit(int n)
+{
+  int rd = 0;
+
+  while (n > 0)
+  {
+    int ld = n % 10;
+    rd = (rd * 10) + ld;
+    n = n / 10;
+  }
+  cout << rd;
+}
+
+void palindrome(int n)
+{
+  int temp = n;
+  int revDig = 0;
+
+  while (n > 0)
+  {
+    int ld = n % 10;
+    revDig = (revDig * 10) + ld;
+    n = n / 10;
+  }
+  if (temp == revDig)
+    cout << "true";
+  else
+    cout << "false";
+}
 int main()
 {
 
   int integer;
   cin >> integer;
 
-  countDigit(integer);
+  // countDigit(integer);
+  // revDigit(integer);
+  palindrome(integer);
   return 0;
 }
