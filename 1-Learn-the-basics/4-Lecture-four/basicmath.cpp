38cd8d40a452e83192869983f0504876252088c2
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-07-30 13:16:24 +0530

    optimized the code for print all divisor

--- 1-Learn-the-basics/4-Lecture-four/basicmath.cpp
+++ 1-Learn-the-basics/4-Lecture-four/basicmath.cpp
@@ -68,13 +68,27 @@ void armstrongNum(int n)
 
 void printAllDivisors(int n)
 {
-  for (int i = 1; i <= n; i++)
+  /*for (int i = 1; i <= n; i++)
   {
     if (n % i == 0)
     {
       cout << i << " ";
     };
+
+    // time complexity - O(N)
+  }*/
+  vector<int> ls;
+  for (int i = 1; i <= sqrt(n); i++)
+  {
+    if (n % i == 0)
+      ls.push_back(i);
+    if (n / i != i)
+      ls.push_back(n / i);
   }
+
+  sort(ls.begin(), ls.end());
+  for(auto it : ls) cout << it << " ";
+  // time complexity - O(N/2)
 }
 int main()
 {
