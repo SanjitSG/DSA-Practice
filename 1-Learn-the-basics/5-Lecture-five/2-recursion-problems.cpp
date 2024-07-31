91ae5e28e058980387991ff3a1ad213fd1c8d2a0
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-08-01 01:30:49 +0530

    basic recursion problems

--- 1-Learn-the-basics/5-Lecture-five/2-recursion-problems.cpp
+++ 1-Learn-the-basics/5-Lecture-five/2-recursion-problems.cpp
@@ -9,21 +9,46 @@ void printName(int i, int n)
   cout << "Sanjit" << " ";
   printName(i + 1, n);
 }
-void printNum(int i, int n)
+void printNum1(int i, int n)
 {
   if (i > n)
     return;
   cout << i << " ";
 
-  printNum(i + 1, n);
+  printNum1(i + 1, n);
 }
 
+void printNum2(int i, int n)
+{
+  if (i < 1)
+    return;
+  cout << i << endl;
+  printNum2(i - 1, n);
+}
+void backtracking1(int i, int n)
+{
+  if (i < 1)
+    return;
+  backtracking1(i - 1, n);
+  cout << i << endl;
+}
+
+void backtracking2(int i, int n)
+{
+  if (i > n)
+    return;
+  backtracking2(i + 1, n);
+  cout << i << endl;
+}
 int main()
 {
   int n;
   cin >> n;
-  printName(1, n);
-  // printNum(1, n);
+  // printName(1, n);
+  // printNum1(1, n); //1 -> N
+  printNum2(n, n); // N -> 1
+  // backtracking1(n, n); // 1 -> N
+  // backtracking2(1, n); // N -> 1
 
   return 0;
 }
