1273602e53c114f9307587b72965f7d3c3d5914e
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-08-01 18:34:52 +0530

    factorial using recursion

--- 1-Learn-the-basics/5-Lecture-five/3-sum.cpp
+++ 1-Learn-the-basics/5-Lecture-five/3-sum.cpp
@@ -13,6 +13,7 @@ void sum1(int i, int sum)
   return sum1(i - 1, sum + i);
 }
 
+// *2. functional way - function returns a value
 int sum2(int n)
 {
   if (n == 0)
@@ -21,11 +22,33 @@ int sum2(int n)
   return n + sum2(n - 1);
 }
 
-// *2. functional way - function returns a value
+// *3 factorial of a number using functional method
+int fact(int n)
+{
+  if (n < 1)
+    return 1;
+  return n * fact(n - 1);
+}
+
+//* factorial using parameterized method
+void fact1(int i, int res)
+{
+  if (i < 1)
+  {
+    cout << "fact is " << res;
+    return;
+  }
+  fact1(i - 1, res * i);
+}
+
 int main()
 {
   int n;
   cin >> n;
   sum1(n, 0);
-  cout << sum2(n);
+  cout << sum2(n) << endl;
+  cout << "factorial of " << n << " is " << fact(n) << endl;
+
+  fact1(n, 1);
+  return 0;
 }
