1a105fbf657be72f2279e2c456dc383dfaf87cc4
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-08-03 17:56:47 +0530

    palindrome

--- 1-Learn-the-basics/5-Lecture-five/4-reverse-array.cpp
+++ 1-Learn-the-basics/5-Lecture-five/4-reverse-array.cpp
@@ -10,20 +10,38 @@ void f(int i, int arr[], int n)
   f(i + 1, arr, n);
 }
 
+bool palindrome(int i, string &s)
+{
+  if (i > s.size() / 2)
+  {
+    return true;
+  }
+  if (s[i] != s[s.size() - i - 1])
+  {
+    return false;
+  }
+  return palindrome(i + 1, s);
+}
 int main()
 {
   int n;
   cin >> n;
   int arr[n];
+  string s = "madam";
 
   for (int i = 0; i < n; i++)
     cin >> arr[i];
 
-  f(0, arr, n);
+  // using two pointers
+  //  f(0, arr, n);
 
-  for (int i = 0; i < n; i++)
-  {
-    cout << arr[i] << " ";
-  }
+  // palindrome;
+
+  cout << palindrome(0, s);
+
+  // for (int i = 0; i < n; i++)
+  // {
+  //   cout << arr[i] << " ";
+  // }
   return 0;
 }
