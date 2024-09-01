6d0dae33361873721cfa0c73530a86f17aeda0f9
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-09-01 21:55:39 +0530

    largest odd num in a string

---
+++ 4-Strings/Basics/largest-odd-in-string.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// two pointer method and substr
+string largestOdd(string &str)
+{
+  // length
+  int n = str.length();
+
+  // find first odd num from the end of the str
+  int start = 0;
+  int end = 0;
+
+  for (int i = n - 1; i >= 0; i--)
+  {
+    // finding last odd index
+    if (str[i] - '0' % 2 == 1)
+    {
+      end = i;
+      break;
+    }
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    // finding first non-zero index
+    if (str[i] - '0' != 0)
+    {
+      start = i;
+      break;
+    }
+  }
+
+  return str.substr(start, end - start + 1);
+}
+int main()
+{
+
+  string str;
+  cin >> str;
+
+  cout << largestOdd(str);
+  return 0;
+}
